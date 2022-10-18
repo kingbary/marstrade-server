0b83a9585d5f83e4d983ceacecb021f4e7d54512
gnobu <inyangubong17@gmail.com>
2022-10-18 11:37:20 +0100

    signup controller added

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,7 @@
 import e from "express"
 import asyncHandler from "express-async-handler"
-import bcrypt from "bcrypt"
 import { IMongoService } from "../services/db.service"
+import { IEncryption } from "../services/encryption.service"
 
 
 export interface IAuth {
@@ -11,13 +11,38 @@ export interface IAuth {
 
 export class Auth implements IAuth {
     private readonly persistence
+    private readonly cryptService
 
-    constructor(persistence: IMongoService) {
+    constructor(persistence: IMongoService, cryptService: IEncryption) {
         this.persistence = persistence
+        this.cryptService = cryptService
     }
 
-    signup = asyncHandler(async (req, res) => { })
-    
+    signup = asyncHandler(async (req, res) => {
+        const { firstName, lastName, email, password, rePassword } = req.body
+        const duplicate = await this.persistence.getUser(email)
+
+        // confirm data
+        if (!email || !firstName || !lastName || !password || password === rePassword) {
+            res.status(400).json({ message: 'All fields are required' })
+            return
+        }
+
+        // Check for duplicate
+        if (duplicate) {
+            res.status(409).json({ message: 'Duplicate mail' })
+            return
+        }
+        const hashPass = await this.cryptService.encrypt(password)
+        const newUser = await this.persistence.createUser({ firstName, lastName, email, password: hashPass })
+
+        if (!newUser) { // created successfully
+            res.status(201).json({ message: `Account created succesfully` })
+        } else {
+            res.status(400).json({ message: 'Invalid user data received' })
+        }
+    })
+
     login = asyncHandler(async (req, res) => {
         const { email, password } = req.body
         const user = await this.persistence.getUser(email)
@@ -26,14 +51,14 @@ export class Auth implements IAuth {
             res.status(400).json({ message: 'User not found' })
             return
         }
-        
-        const match = await bcrypt.compare(password, user.password)
-        
+
+        // ensure correct password
+        const match = await this.cryptService.compare(password, user.password)
+
         if (!match) {
             res.status(401).json({ message: 'Invalid details' })
-            return
+        } else {
+            res.json(user)
         }
-
-        // retuen the user details JSON
     })
 }
--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,12 +1,20 @@
 import User, { IUser } from "../models/user.model";
 
 export interface IMongoService {
-    getUser: (id: string) => Promise<IUser | null>
+    createUser(arg0: IUser): Promise<IUser | null>;
+    getUser(id: string): Promise<IUser | null>
 }
 
 export class MongoService implements IMongoService {
     async getUser(email: string) {
-        const user = await User.findOne<IUser>({ email })
+        const user = await User.findOne<IUser>({ email }).lean().exec()
+        return user
+    }
+
+    async createUser(userDetails: IUser) {
+        const user = new User<IUser>(userDetails)
+        await user.save()
+        
         return user
     }
 }
