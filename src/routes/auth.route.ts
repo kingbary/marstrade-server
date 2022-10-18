e80a7cb29d59d6cf50eaf733b9b249ce5666b2be
gnobu <inyangubong17@gmail.com>
2022-10-18 11:30:43 +0100

    crypt service added

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,7 +3,9 @@ const router = express.Router()
 
 import { Auth, IAuth } from '../controller/auth.controller'
 import { MongoService } from '../services/db.service'
-const auth: IAuth = new Auth(new MongoService())
+import { Encryption } from '../services/encryption.service'
+
+const auth: IAuth = new Auth(new MongoService(), new Encryption())
 
 router.post('/signup', auth.signup)
 
