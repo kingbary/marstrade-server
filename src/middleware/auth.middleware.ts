925daceeefd734c1b13b1d1336ff25667a5d6444
gnobu <inyangubong17@gmail.com>
2022-10-18 18:31:13 +0100

    added validation for signup data

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,7 +5,7 @@ export const signUpValidator = [
     check("lastName").trim().isLength({ min: 1 }).withMessage("Name must be more than one character long"),
     check("email").isEmail().withMessage("Please input a valid email").normalizeEmail(),
     check("password").isLength({ min: 6 }).withMessage("Password must be more than five characters long"),
-    check("retypePassword").custom((val, { req }) => {
+    check("rePassword").custom((val, { req }) => {
         if (req.body.password !== val) {
             throw new Error("Passwords must match")
         }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 const router = express.Router()
 
+import { signUpValidator } from '../middleware/auth.middleware'
+
 import { Auth, IAuth } from '../controller/auth.controller'
 import { MongoService } from '../services/db.service'
 import { Encryption } from '../services/encryption.service'
-
 const auth: IAuth = new Auth(new MongoService(), new Encryption())
 
-router.post('/signup', auth.signup)
+router.post('/signup', signUpValidator, auth.signup)
 
 router.post('/login', auth.login)
 
