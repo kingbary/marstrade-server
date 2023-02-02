83b06d8807f6362165edd4aae012bc419e322303
Kingsley <kingsleyakwa@gmail.com>
2023-02-02 15:21:02 +0100

    feat: fixed bug

--- dist/models/investment.model.js
+++ dist/models/investment.model.js
@@ -4,18 +4,22 @@ exports.packageConverter = void 0;
 const mongoose_1 = require("mongoose");
 const types_1 = require("./types");
 exports.packageConverter = {
-    'AGRICULTURE': 5,
-    'FOREX': 2.5,
-    'STOCK': 2.5,
-    'INHERITANCE': 7.5,
-    'ENERGY': 7.5,
-    'CRYPTOCURRENCY': 2.5,
-    'METAL': 7.5,
-    'REAL_ESTATE': 5
+    AGRICULTURE: 5,
+    FOREX: 2.5,
+    STOCK: 2.5,
+    INHERITANCE: 7.5,
+    ENERGY: 7.5,
+    CRYPTOCURRENCY: 2.5,
+    METAL: 7.5,
+    REAL_ESTATE: 5,
 };
 const investmentSchema = new mongoose_1.Schema({
-    investor: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'User' },
-    transaction: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'Transaction' },
+    investor: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: "User" },
+    transaction: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        required: true,
+        ref: "Transaction",
+    },
     investmentPlan: { type: String, required: true },
     investmentAmount: { type: Number, required: true },
     investmentPackage: { type: String, required: true },
@@ -25,14 +29,19 @@ const investmentSchema = new mongoose_1.Schema({
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-    timestamps: true
+    timestamps: true,
 });
-investmentSchema.virtual('ROI')
-    .get(function () {
-    const timems = this.createdAt ? Date.now() - new Date(this.createdAt).getTime() : 0;
-    const daysPassed = Math.round(timems / (1000 * 60 * 60 * 24));
-    const interest = this.investmentAmount * daysPassed * exports.packageConverter[this.investmentPackage] * 0.01;
+investmentSchema.virtual("ROI").get(function () {
+    const timems = this.createdAt
+        ? Date.now() - new Date(this.createdAt).getTime()
+        : 0;
+    let daysPassed = Math.round(timems / (1000 * 60 * 60 * 24));
+    daysPassed = daysPassed > 7 ? 7 : daysPassed;
+    const interest = this.investmentAmount *
+        daysPassed *
+        exports.packageConverter[this.investmentPackage] *
+        0.01;
     return interest;
 });
-const Investment = (0, mongoose_1.model)('Investment', investmentSchema);
+const Investment = (0, mongoose_1.model)("Investment", investmentSchema);
 exports.default = Investment;
--- src/models/investment.model.ts
+++ src/models/investment.model.ts
@@ -1,42 +1,52 @@
-import { Schema, model } from "mongoose"
-import { IInvestment, STATUS } from "./types"
+import { Schema, model } from "mongoose";
+import { IInvestment, STATUS } from "./types";
 
 export const packageConverter: { [key: string]: number } = {
-    'AGRICULTURE': 5,
-    'FOREX': 2.5,
-    'STOCK': 2.5,
-    'INHERITANCE': 7.5,
-    'ENERGY': 7.5,
-    'CRYPTOCURRENCY': 2.5,
-    'METAL': 7.5,
-    'REAL_ESTATE': 5
-}
+  AGRICULTURE: 5,
+  FOREX: 2.5,
+  STOCK: 2.5,
+  INHERITANCE: 7.5,
+  ENERGY: 7.5,
+  CRYPTOCURRENCY: 2.5,
+  METAL: 7.5,
+  REAL_ESTATE: 5,
+};
 
 const investmentSchema = new Schema<IInvestment>(
-    {
-        investor: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-        transaction: { type: Schema.Types.ObjectId, required: true, ref: 'Transaction' },
-        investmentPlan: { type: String, required: true },
-        investmentAmount: { type: Number, required: true },
-        investmentPackage: { type: String, required: true },
-        status: { type: String, default: STATUS.PENDING },
-        isActive: { type: Boolean, default: true },
-        // ROI: { type: Number, default: 0 }
+  {
+    investor: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    transaction: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Transaction",
     },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true },
-        timestamps: true
-    }
-)
+    investmentPlan: { type: String, required: true },
+    investmentAmount: { type: Number, required: true },
+    investmentPackage: { type: String, required: true },
+    status: { type: String, default: STATUS.PENDING },
+    isActive: { type: Boolean, default: true },
+    // ROI: { type: Number, default: 0 }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
 
-investmentSchema.virtual('ROI')
-    .get(function () {
-        const timems = this.createdAt ? Date.now() - new Date(this.createdAt).getTime() : 0
-        const daysPassed = Math.round(timems / (1000 * 60 * 60 * 24))
-        const interest = this.investmentAmount * daysPassed * packageConverter[this.investmentPackage] * 0.01
-        return interest
-    })
+investmentSchema.virtual("ROI").get(function () {
+  const timems = this.createdAt
+    ? Date.now() - new Date(this.createdAt).getTime()
+    : 0;
+  let daysPassed = Math.round(timems / (1000 * 60 * 60 * 24));
+  daysPassed = daysPassed > 7 ? 7 : daysPassed;
+  const interest =
+    this.investmentAmount *
+    daysPassed *
+    packageConverter[this.investmentPackage] *
+    0.01;
+  return interest;
+});
 
-const Investment = model<IInvestment>('Investment', investmentSchema)
-export default Investment
+const Investment = model<IInvestment>("Investment", investmentSchema);
+export default Investment;
